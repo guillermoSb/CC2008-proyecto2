f3d9c4af8d2a173666f8106073a77881452f7a1a
robertriosm <87707371+robertriosm@users.noreply.github.com>
2021-09-30 18:30:43 -0600

    Update Usuario.java

--- models/Usuario.java
+++ models/Usuario.java
@@ -1,4 +1,12 @@
 package models;
 public class Usuario {
-    
+    private String nombre;
+    private String apellido;
+    private String departamento;
+    private String municipio;
+    private int zona;
+    private int puntos;
+    private ArrayList<Consumo> consumos;
+
 }
+
