3863aa1ce3f3c55136e00b3893cfac6f3000eafd
robertriosm <87707371+robertriosm@users.noreply.github.com>
2021-09-30 19:40:38 -0600

    Update Usuario.java

--- models/Usuario.java
+++ models/Usuario.java
@@ -11,5 +11,59 @@ public class Usuario {
     private int puntos;
     private ArrayList<Consumo> consumos;
 
-}
+    public String getApellido() {
+        return apellido;
+    }
 
+    public ArrayList<Consumo> getConsumos() {
+        return consumos;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public int getZona() {
+        return zona;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setConsumos(ArrayList<Consumo> consumos) {
+        this.consumos = consumos;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public void setZona(int zona) {
+        this.zona = zona;
+    }
+}
