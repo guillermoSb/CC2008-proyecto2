9c375544f634411fc64731cd0ac9766c70a67342
robertriosm <87707371+robertriosm@users.noreply.github.com>
2021-09-30 19:17:15 -0600

    Update Consumo.java

--- models/Consumo.java
+++ models/Consumo.java
@@ -1,8 +1,39 @@
 package models;
 
+import java.util.Date;
+
 public class Consumo {
     private Producto producto;
     private int cantidad;
-    private String fecha;
-    
+    private Date fecha;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Consumo(int cantidad, Producto producto) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+    }
+
 }
