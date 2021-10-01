25fade747c8fe23b4f96116be6f2ff22c7a4c843
guillermoSb <guillermo.santosba@gmail.com>
2021-09-30 18:04:39 -0600

    Added centro reciclaje class

--- FileManager.java
+++ FileManager.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+
+import models.CentroReciclaje;
+import models.Producto;
+import models.Usuario;
+
+/**
+ * Se encarga de manejar la persitencia de datos.
+ */
 public class FileManager {
+    private static String path = "files";  // Path a los archivos que vamos a utilizar
     
+    
+    public static Usuario cargarUsuario() {
+        return null;
+    }
+
+    public static ArrayList<Producto> cargarProductos() {
+        return null;
+    }
+
+    public static ArrayList<CentroReciclaje> cargarCentroReciclaje() {
+        return null;
+    }
 }
