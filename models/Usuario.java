15cc4ba92307fe41fdd9caf3b21cc8b8731ad986
Roberto Rios <rio20979@uvg.edu.gt>
2021-09-30 18:45:36 -0600

    Cambios realizados

--- models/Usuario.java
+++ models/Usuario.java
@@ -1,4 +1,14 @@
 package models;
+
+import java.util.ArrayList;
+
 public class Usuario {
-    
+    private String nombre;
+    private String apellido;
+    private String departamento;
+    private String municipio;
+    private int zona;
+    private int puntos;
+    private ArrayList<Consumo> consumos;
+
 }
