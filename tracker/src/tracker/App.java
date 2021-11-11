82362999765557be5712d922b85ffe37100254f5
Guillermo <san191517@uvg.edu.gt>
2021-11-11 01:37:56 -0600

    Created the JavaFX Project.
    Created a project in JAVAFX

---
+++ tracker/src/tracker/App.java
@@ -0,0 +1,17 @@
+package tracker;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        System.out.println("Starting App");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
