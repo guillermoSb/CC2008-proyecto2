daa53c5797900a2efecb11969a95af9098656d6f
robertriosm <87707371+robertriosm@users.noreply.github.com>
2021-11-26 21:21:57 -0600

    comentario

--- trackingBasura/src/main/java/interfaces/GoBack.java
+++ trackingBasura/src/main/java/interfaces/GoBack.java
@@ -1,7 +1,9 @@
 package interfaces;
 
 import java.io.IOException;
-
+/**
+* esta interfaz sirve para implementarse en otras clases como el boton de regresar
+*/
 public interface GoBack {
     public void goBack() throws IOException;
 }
